f057648befbc45d9d3ef13237c7190874c298b2e
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2024-02-18 17:40:38 -0500

    Render form submission for users

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,18 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  # Routes for signup, login, and logout
+  get '/signup' => 'users#new' 
+  # // render the sign up form 
+  post '/users' => 'users#create'
+  # //handle submission 
+  
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
